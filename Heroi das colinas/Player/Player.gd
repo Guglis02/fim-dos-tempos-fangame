8d27ec26ff70e98c7be8a9a498759858d2f1cbc2
Guglis02 <gustavomf02@gmail.com>
2021-04-12 16:53:26 -0300

    UI adicionada
    Além disso, o jogador agora fica intangível durante o rolamento, e árvores foram adicionadas

--- Heroi das colinas/Player/Player.gd
+++ Heroi das colinas/Player/Player.gd
@@ -89,6 +89,7 @@ func attack_animation_finished():
 
 
 func _on_Hurtbox_area_entered(_area):
-	stats.health -= 1;
-	hurtbox.start_invincibility(0.5);
-	hurtbox.create_hit_effect();
+	if state != ROLL:
+		stats.health -= 1;
+		hurtbox.start_invincibility(0.5);
+		hurtbox.create_hit_effect();
